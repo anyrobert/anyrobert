772b10bba238b1fc674131e771d5ee86d210542a
Robert Gabriel <anyrobert@outlook.com.br>
2022-01-09 10:28:32 -0300

    chore: add tailwind customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
- content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
+    extend: {
+      backgroundImage: {
+        profile: `url('/anyrobert.jpg')`,
+      },
+      colors: {
+        primary: '#CC0F11',
+        'light-primary': '#FA4F59',
+        secondary: '#2F4858',
+        'light-secondary': '#00A3CB',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
