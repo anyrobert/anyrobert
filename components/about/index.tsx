fa9812f3f1ad37dd96a6ddf823668168c2086e73
Robert Gabriel <anyrobert@outlook.com.br>
2022-05-18 17:18:54 -0300

    feat: add animations

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,29 +1,42 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export const About = () => {
-    return (
-        <div className="container px-4 mx-auto">
-            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-                <div className="lg:px-4 lg:mt-12 ">
-                    <h1 className=" rola text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                        Hey there,
-                    </h1>
-                    <div className=" mt-6 text-gray-800 dark:text-white">
-                        <p className="mb-4">
-                            {` Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a
-                            type specimen book.`}
-                        </p>
-                    </div>
-                </div>
-                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
-                    <div className="card-zoom">
-                        <div className="card-zoom-image bg-profile" />
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container px-4 mx-auto">
+      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+        <motion.article
+          className="lg:px-4 lg:mt-12 h-max"
+          initial={{ opacity: 0, skew: 30, x: -250 }}
+          whileHover={{ scale: 1.05 }}
+          animate={{ opacity: 1, skew: 0, x: 0 }}
+        >
+          <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+            Robert Gabriel,
+          </h1>
+          <h3 className="lg:text-2xl font-bold text-gray-900  dark:text-white">
+            Developer
+          </h3>
+          <div className=" mt-6 text-gray-800 dark:text-white">
+            <p className="mb-4 text-lg flex">
+              Freelance Web Developer based in Curitiba, Brazil. ☕
+              <br />
+              Passionate about technology and design area, clean code and agile
+              ways of work enthusiast.
+              <br />
+            </p>
+          </div>
+        </motion.article>
+        <motion.div
+          className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10"
+          initial={{ opacity: 0, skew: 30, x: 250 }}
+          animate={{ opacity: 1, skew: 0, x: 0 }}
+        >
+          <div className="card-zoom m-auto">
+            <div className="card-zoom-image bg-profile" />
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  )
 }
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,40 +1,59 @@
-import React from 'react';
+import React from 'react'
+import { motion } from 'framer-motion'
 
 export const Footer = () => {
-    return (
-        <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
-            <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
-                <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
-                <div className="flex flex-col justify-between lg:flex-row items-center">
-                    <p className='hover:text-primary'>¯\_(ツ)_/¯</p>
-                    <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
-                        <a
-                            href="https://twitter.com/anyrobert_"
-                            className={'transition-colors hover:text-primary'}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Twitter
-                        </a>
-                        <a
-                            href="https://www.linkedin.com/in/robert-gabriel-cardoso"
-                            className={'transition-colors hover:text-primary'}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            LinkedIn
-                        </a>
-                        <a
-                            href="https://github.com/anyrobert"
-                            className={'transition-colors hover:text-primary'}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            GitHub
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
+      <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
+        <motion.div
+          className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"
+          initial={{ opacity: 0, skew: 30, x: 250 }}
+          animate={{ opacity: 1, skew: 0, x: 0 }}
+        />
+        <div className="flex flex-col justify-between lg:flex-row items-center">
+          <motion.p
+            className="hover:text-primary cursor-default"
+            initial={{ opacity: 0, skew: 30, x: -250 }}
+            animate={{ opacity: 1, skew: 0, x: 0 }}
+            whileHover={{ scale: 1.1, skew: 5 }}
+          >
+            ¯\_(ツ)_/¯
+          </motion.p>
+          <motion.div
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0"
+          >
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              href="https://twitter.com/anyrobert_"
+              className={'transition-colors hover:text-primary'}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </motion.a>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              href="https://www.linkedin.com/in/robert-gabriel-cardoso"
+              className={'transition-colors hover:text-primary'}
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </motion.a>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              href="https://github.com/anyrobert"
+              className={'transition-colors hover:text-primary'}
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </motion.a>
+          </motion.div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
